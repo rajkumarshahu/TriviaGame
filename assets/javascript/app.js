618b951861f291c39d92634438198f727ebdad07
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-07-10 23:57:51 -0400

    container and placeholders created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,62 +1,101 @@
 const trivia = [
-    {
-        id: 1,
-        question: "Where did Charlie Chaplin spend his remaining years after being exiled from the U.S. in 1953?",
-        choices: ["England", "Switzerland", "Germany", "Holland"],
-        answer: "Switzerland"
-    },
-    {
-        id: 2,
-        question: "One main reason Charlie was exiled from the U.S. in 1953 was because he refused ______ ?",
-        choices: ["to pay his taxes", "an Academy Award", "U.S. citizenship", "to meet the President"],
-        answer: "U.S. citizenship"
-    },
-    {
-        id: 3,
-        question: "Which actor portrayed Charlie in the film 'Chaplin'?",
-        choices: ["Robert Downey Jr.", "Robert Downey Jr.", "Jim Carrey", " Joseph Fines"],
-        answer: "Robert Downey Jr."
-    },
-    {
-        id: 4,
-        question: "In 'The Great Dictator', what famous leader did Charlie portray?",
-        choices: ["Napoleon", "Adolf Hitler", "Caesar", "George Washington"],
-        answer: "Adolf Hitler"
-    },
-    {
-        id: 5,
-        question: "How many times was Charlie married?",
-        choices: ["5", "7", "6", "4"],
-        answer: "4"
-    },
-    {
-        id: 6,
-        question: "What company did Charlie co-found in 1919?",
-        choices: ["Columbia Pictures", "Paramount", "United Artists", "Warner Brothers"],
-        answer: "United Artists"
-    },
-    {
-        id: 7,
-        question: "Which one of these isn't a Chaplin film?",
-        choices: ["A Women of Paris", "City Lights", " The Life a Tramp", " The Kid"],
-        answer: "The Life a Tramp"
-    },
-    {
-        id: 8,
-        question: "Where was Charlie Chaplin born?",
-        choices: ["United States", "England", "France", "Australia"],
-        answer: "England"
-    },
-    {
-        id: 9,
-        question: "Charlie Chaplin's first solo appearance was in which film?",
-        choices: ["One A. M.", "The Bank", "The Tramp", "The Adventurer "],
-        answer: "One A. M."
-    },
-    {
-        id: 10,
-        question: "What other silent film funny-man appeared in \"Limelight\" with Chaplin?",
-        choices: ["Harold Lloyd", "Buster Keaton", "John Gilbert", "William Haines"],
-        answer: "Buster Keaton"
-    },
+  {
+    id: 1,
+    question:
+      "Where did Charlie Chaplin spend his remaining years after being exiled from the U.S. in 1953?",
+    choices: ["England", "Switzerland", "Germany", "Holland"],
+    answer: "Switzerland",
+    gif: "assets/images/gif-01.gif",
+  },
+  {
+    id: 2,
+    question:
+      "One main reason Charlie was exiled from the U.S. in 1953 was because he refused ______ ?",
+    choices: [
+      "to pay his taxes",
+      "an Academy Award",
+      "U.S. citizenship",
+      "to meet the President",
+    ],
+    answer: "U.S. citizenship",
+    gif: "assets/images/gif-02.gif",
+  },
+  {
+    id: 3,
+    question: "Which actor portrayed Charlie in the film 'Chaplin'?",
+    choices: [
+      "Robert Downey Jr.",
+      "Robert Downey Jr.",
+      "Jim Carrey",
+      " Joseph Fines",
+    ],
+    answer: "Robert Downey Jr.",
+    gif: "assets/images/gif-03.gif",
+  },
+  {
+    id: 4,
+    question:
+      "In 'The Great Dictator', what famous leader did Charlie portray?",
+    choices: ["Napoleon", "Adolf Hitler", "Caesar", "George Washington"],
+    answer: "Adolf Hitler",
+    gif: "assets/images/gif-04.gif",
+  },
+  {
+    id: 5,
+    question: "How many times was Charlie married?",
+    choices: ["5", "7", "6", "4"],
+    answer: "4",
+    gif: "assets/images/gif-05.gif",
+  },
+  {
+    id: 6,
+    question: "What company did Charlie co-found in 1919?",
+    choices: [
+      "Columbia Pictures",
+      "Paramount",
+      "United Artists",
+      "Warner Brothers",
+    ],
+    answer: "United Artists",
+    gif: "assets/images/gif-06.gif",
+  },
+  {
+    id: 7,
+    question: "Which one of these isn't a Chaplin film?",
+    choices: [
+      "A Women of Paris",
+      "City Lights",
+      " The Life a Tramp",
+      " The Kid",
+    ],
+    answer: "The Life a Tramp",
+    gif: "assets/images/gif-07.gif",
+  },
+  {
+    id: 8,
+    question: "Where was Charlie Chaplin born?",
+    choices: ["United States", "England", "France", "Australia"],
+    answer: "England",
+    gif: "assets/images/gif-08.gif",
+  },
+  {
+    id: 9,
+    question: "Charlie Chaplin's first solo appearance was in which film?",
+    choices: ["One A. M.", "The Bank", "The Tramp", "The Adventurer "],
+    answer: "One A. M.",
+    gif: "assets/images/gif-09.gif",
+  },
+  {
+    id: 10,
+    question:
+      'What other silent film funny-man appeared in "Limelight" with Chaplin?',
+    choices: [
+      "Harold Lloyd",
+      "Buster Keaton",
+      "John Gilbert",
+      "William Haines",
+    ],
+    answer: "Buster Keaton",
+    gif: "assets/images/gif-10.gif",
+  },
 ];
